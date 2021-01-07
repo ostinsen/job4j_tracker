257e615ea77d36f32e0a199b4072630d5cb9cfc3
ostinsen <ostinsen@yandex.ru>
2021-01-07 18:04:56 +0300

    1.1. Создание объекта [#434948]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,12 +1,14 @@
 package ru.job4j.tracker;
 
+
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class TrackerTest {
-    @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+ public class TrackerTest {
+
+     @Test
+     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
         Item item = new Item();
         item.setName("test1");
