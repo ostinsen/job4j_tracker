7b24ad06e169ca09cbcdd6fa863916d8fc589dc1
ostinsen <ostinsen@yandex.ru>
2021-01-10 14:15:15 +0300

    2. Перегрузка конструктора [#434960]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,19 +4,30 @@ public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
-    }
+    public Item(){
 
-    public void setId(int id) {
+    }
+    public Item(int id){
         this.id = id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Item(int id, String name){
+        this.id = id;
         this.name = name;
     }
+
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
