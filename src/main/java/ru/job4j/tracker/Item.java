e6b0f73afc1ca7417b57bce65ce2eb5c48dfffbf
ostinsen <ostinsen@yandex.ru>
2021-01-13 20:34:16 +0300

    9. toString [#434976]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,6 +7,14 @@ public class Item {
     private int id;
     private String name;
 
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
     public Item(){
 
     }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,12 +8,15 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
        String dateNow = item.getCreated().format(formatter);
-        System.out.println(dateNow);
+       // System.out.println(dateNow);
 
         Tracker tracker  = new Tracker();
         tracker.add(new Item(10, "massa"));
         tracker.add(item);
         Item it = tracker.findById(1);
-        System.out.println(it.getId() + " " + it.getName());
+        //System.out.println(it.getId() + " " + it.getName());
+
+        Item item2 = new Item(1234, "Example2");
+        System.out.println(item2.toString());
     }
 }
