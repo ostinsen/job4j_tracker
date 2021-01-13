1d5e35ac2bcb5fd63460f05718a00fb5679bbbe8
ostinsen <ostinsen@yandex.ru>
2021-01-13 09:51:04 +0300

    5. Tracker - хранилище [#434977]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,10 +4,16 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
        String dateNow = item.getCreated().format(formatter);
         System.out.println(dateNow);
+
+        Tracker tracker  = new Tracker();
+        tracker.add(new Item(10, "massa"));
+        tracker.add(item);
+        Item it = tracker.findById(1);
+        System.out.println(it.getId() + " " + it.getName());
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,4 +24,21 @@ public class Tracker {
         }
         return rsl;
     }
+    public Item[] findAll(){
+        Item[] itemsAll = Arrays.copyOf(items, size);
+            return itemsAll;
+    }
+
+    public Item[] findByName (String key){
+        int count = 0;
+        Item[] itemsName = new Item[100];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getName().equals(key)){
+                itemsName[count++] = items[i];
+            }
+            itemsName = Arrays.copyOf(itemsName, count);
+        }
+        return itemsName;
+    }
+
 }
