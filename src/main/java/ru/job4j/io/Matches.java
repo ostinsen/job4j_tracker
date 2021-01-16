08fce426764e475150ff2b276250f9d0f3164660
ostinsen <ostinsen@yandex.ru>
2021-01-16 17:24:14 +0300

    2. Scanner и чтение числа из консоли. [#434898]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,50 +3,32 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
-    public static void main ( String[] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean run = true;
         int mathes = 11;
-        while (run) {
-            boolean bo1 = true;
-            while (bo1) {
-                System.out.print("ПЕрвый игрок берет спички от 1 до 3 ");
-                int select = Integer.valueOf(input.nextLine());
-                if (select >= 1 && select <= 3) {
-
-                    bo1 = false;
-                    mathes = mathes - select;
-                    System.out.println("ОСталось спичек - " + mathes);
-                    if (mathes <= 0) {
-                        System.out.println("ПЕрвый игрок выиграл");
-                        run = false;
-                        break;
-                    }
-                } else {
-                    System.out.println(" Первый игрок не корректно ввел значение");
-                    System.out.println("осталось спичек " + mathes);
-                }
+        int count = 1;
+        System.out.println(" Первый игрок ходит, чтобы начать игру:");
+        while (true) {
+            System.out.print("Введите от 1 до 3 ");
+            int select = Integer.valueOf(input.nextLine());
+            if (select >= 1 && select <= 3 && count % 2 != 0) {
+                mathes = mathes - select;
+                if (mathes <= 0) break;
+                count++;
+                System.out.println("осталось спичек " + mathes + " Очередь второго игрока");
+            } else if (select >= 1 && select <= 3 && count % 2 == 0) {
+                mathes = mathes - select;
+                if (mathes <= 0) break;
+                count++;
+                System.out.println("осталось спичек " + mathes + " Очередь первого игрока");
+            } else {
+                System.out.println("Повторите ввод ");
             }
-            if (mathes <= 0) break;
-            boolean bo = true;
-            while (bo) {
-                System.out.println("Второй игрок берет спички от 1 до 3 ");
-            int select1 = Integer.valueOf(input.nextLine());
-            if (select1 >= 1 && select1 <= 3) {
-                bo = false;
-            mathes = mathes - select1;
-            System.out.println("ОСталось спичек - " + mathes);
-            if (mathes <= 0) {
-                System.out.println("Второй игрок выиграл");
-                run = false;
-                break;
-            }
-            }else {
-                System.out.println(" Второй игрок не корректно ввел значение");
-                System.out.println("осталось спичек - " + mathes);
-            }
-            }
-
+        }
+        if (count % 2 != 0) {
+            System.out.println("Выиграл первый игрок");
+        } else {
+            System.out.println("Выиграл второй игрок");
         }
     }
 }
