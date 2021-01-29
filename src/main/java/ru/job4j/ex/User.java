16bd5ba4fa42c0894bb026248d0753b067a7f104
ostinsen <ostinsen@yandex.ru>
2021-01-29 18:59:57 +0300

    0.5. Иерархия исключений и множественный catch. [#434920]

---
+++ src/main/java/ru/job4j/ex/User.java
@@ -0,0 +1,35 @@
+package ru.job4j.ex;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private boolean valid;
+
+    public User(String username, boolean valid) {
+        this.username = username;
+        this.valid = valid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return valid == user.valid &&
+                username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, valid);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,47 @@
+package ru.job4j.ex;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+
+        User user = null;
+
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getUsername().equals(login)) {
+                user = users[i];
+            }
+        }
+        if (user == null) {
+            throw new UserNotFoundException(" User is not found");
+        }
+
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!(user.isValid() || user.getUsername().length() < 3)) {
+                throw new UserInvalidException("User is invalid");
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException unf){
+            System.out.println("Пользователь не валидный");
+        } catch (UserNotFoundException ui){
+            System.out.println("Пользователь не найден");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
