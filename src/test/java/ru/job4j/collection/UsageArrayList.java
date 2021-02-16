bde05b9312f94e4fa4e4c808ab36fc5938526101
ostinsen <ostinsen@yandex.ru>
2021-02-16 21:45:08 +0300

    0. Коллекции, ArrayList, List, Обобщения [#434882]

--- src/test/java/ru/job4j/collection/UsageArrayList.java
+++ src/test/java/ru/job4j/collection/UsageArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
 
-    public static void main( String[] args) {
+    public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Petr");
         arrayList.add("Ivan");
