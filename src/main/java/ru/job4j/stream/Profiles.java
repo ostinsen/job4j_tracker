2f2048f0153fb763cca25d55e6c1e1cdbbcfaada
ostinsen <ostinsen@yandex.ru>
2021-04-11 12:35:53 +0300

    3. Уникальность элементов и сортировка. [#434711]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,24 +1,19 @@
 package ru.job4j.stream;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
     List<Address> collect(List<Profile> profiles){
-        return profiles.stream()
+        return profiles.stream().sorted(Comparator.comparing(profile -> profile.getAddress().getCity()))
                 .map(p -> p.getAddress())
+                .distinct()
                 .collect(Collectors.toList());
-    }
-
-    public static void main(String[] args) {
-        List<Profile> profiles = Arrays.asList(
-                new Profile(new Address("Moskva", "Lenina",24, 6)),
-                new Profile(new Address("Saint Petersburg", "Pushina",44, 16))
-        );
-        System.out.println(new Profiles().collect(profiles));
-
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -15,7 +15,32 @@ public class ProfilesTest {
                 new Profile(new Address("Moskva", "Lenina",24, 6)),
                 new Profile(new Address("Saint Petersburg", "Pushina",44, 16))
         );
+        List<Address> exp = Arrays.asList(
+                new Address("Moskva", "Lenina",24, 6),
+        new Address("Saint Petersburg", "Pushina",44, 16)
+        );
+
+        List<Address> rsl = new Profiles().collect(profiles);
+
+        assertThat(exp, is(rsl));
+    }
+
+    @Test
+    public void whenSortedAddress(){
+        List<Profile> profiles = Arrays.asList(
+                new Profile(new Address("Moskva", "Lenina",24, 6)),
+                new Profile(new Address("Saint Petersbu", "Pushina",44, 16)),
+                new Profile(new Address("Saint Petersburg", "Pushina",44, 16)),
+                new Profile(new Address("Saint Petersbur", "Pushina",44, 16)),
+                new Profile(new Address("Moskva", "Lenina",24, 6)),
+                new Profile(new Address("Saint Petersbu", "Pushina",44, 16)),
+                new Profile(new Address("Saint Petersbu", "Pushina",44, 16)),
+                new Profile(new Address("Moskva", "Lenina",21, 6))
+        );
         List<Address> exp = Arrays.asList(new Address("Moskva", "Lenina",24, 6),
+                new Address("Moskva", "Lenina",21, 6),
+                new Address("Saint Petersbu", "Pushina",44, 16),
+                new Address("Saint Petersbur", "Pushina",44, 16),
                 new Address("Saint Petersburg", "Pushina",44, 16));
 
         List<Address> rsl = new Profiles().collect(profiles);
