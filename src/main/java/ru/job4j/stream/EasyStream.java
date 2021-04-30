60f24620f5191e6dee4f4f74013b4dfa18fbdc6b
ostinsen <ostinsen@yandex.ru>
2021-04-30 20:45:54 +0300

    1. Экзамен. Lambda, Stream API, JDK Features. [#434727]

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -15,34 +15,32 @@ public class EasyStream {
     }
 
     public static EasyStream of(List<Integer> source) {
-        //throw new UnsupportedOperationException();
+
     return new EasyStream(source);
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-       // throw new UnsupportedOperationException();
+
         List<Integer> list2 = new ArrayList<>();
-        for (int i = 0; i < this.list.size(); i++) {
+        for (Integer i  : list) {
             list2.add(fun.apply(this.list.get(i)));
         }
         return new EasyStream(list2);
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-       // throw new UnsupportedOperationException();
+
         List<Integer> list3 = new ArrayList<>();
-        for (Integer i : this.list){
-           if (fun.test(i)){
+        for (Integer i : this.list) {
+            if (fun.test(i)) {
                list3.add(i);
-           } else {
-               continue;
            }
         }
         return new EasyStream(list3);
     }
 
     public List<Integer> collect() {
-        //throw new UnsupportedOperationException();
+
         return List.copyOf(this.list);
     }
 }
